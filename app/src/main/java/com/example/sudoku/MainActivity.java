a4c35b8e5c9cc666c5e2696eddf71cff0964d000
Patel Khushi Sanjivbhai <155909246+Khushi130404@users.noreply.github.com>
2024-05-12 18:55:51 +0530

    Added FullSudoku

--- app/src/main/java/com/example/sudoku/MainActivity.java
+++ app/src/main/java/com/example/sudoku/MainActivity.java
@@ -4,15 +4,35 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
+
+
+    LinearLayout llBoard;
+    TextView cell[][];
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
+        cell = new TextView[9][9];
+        llBoard = findViewById(R.id.llBoard);
+        View inBoard = findViewById(R.id.inBoard);
+
+        if(inBoard instanceof LinearLayout)
+        {
+            LinearLayout innerLayout = (LinearLayout) inBoard;
+            LinearLayout ll11 = innerLayout.findViewById(R.id.ll11);
+        }
+
+        int board[][] = new int[9][9];
+        CompleteSudoku cs = new CompleteSudoku(board);
+
     }
 }
