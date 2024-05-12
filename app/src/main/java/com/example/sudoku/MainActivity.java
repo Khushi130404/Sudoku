5d4b3e30465c547603027067ad8079e7f96c76a8
Patel Khushi Sanjivbhai <155909246+Khushi130404@users.noreply.github.com>
2024-05-12 19:41:02 +0530

    Added FullSudoku

--- app/src/main/java/com/example/sudoku/MainActivity.java
+++ app/src/main/java/com/example/sudoku/MainActivity.java
@@ -13,7 +13,19 @@ public class MainActivity extends Activity {
 
 
     LinearLayout llBoard;
-    TextView cell[][];
+    TextView cell[][][][];
+    int llId[][] = {{R.id.ll11,R.id.ll12,R.id.ll13},
+            {R.id.ll21,R.id.ll22,R.id.ll23},
+            {R.id.ll31,R.id.ll32,R.id.ll33}};
+
+    int blockId[][] = {{R.id.block11,R.id.block12,R.id.block13},
+            {R.id.block21,R.id.block22,R.id.block23},
+            {R.id.block31,R.id.block32,R.id.block33}};
+
+    int celId[][] = {{R.id.cell11,R.id.cell12,R.id.cell13},
+            {R.id.cell21,R.id.cell22,R.id.cell23},
+            {R.id.cell31,R.id.cell32,R.id.cell33}};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -21,18 +33,49 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        cell = new TextView[9][9];
+        cell = new TextView[3][3][3][3];
         llBoard = findViewById(R.id.llBoard);
         View inBoard = findViewById(R.id.inBoard);
 
         if(inBoard instanceof LinearLayout)
         {
             LinearLayout innerLayout = (LinearLayout) inBoard;
-            LinearLayout ll11 = innerLayout.findViewById(R.id.ll11);
+
+            for(int i=0; i<3; i++)
+            {
+                for(int j=0; j<3; j++)
+                {
+                    LinearLayout ll = innerLayout.findViewById(llId[i][j]);
+                    View block = innerLayout.findViewById(blockId[i][j]);
+
+                    if(block instanceof LinearLayout)
+                    {
+                        for(int k=0; k<3; k++)
+                        {
+                            for(int l=0; l<3; l++)
+                            {
+                                cell[i][j][k][l] = block.findViewById(celId[k][l]);
+                            }
+                        }
+                    }
+                }
+            }
         }
 
         int board[][] = new int[9][9];
         CompleteSudoku cs = new CompleteSudoku(board);
-
+        for(int i=0; i<3; i++)
+        {
+            for(int j=0; j<3; j++)
+            {
+                for(int k=0; k<3; k++)
+                {
+                    for(int l=0; l<3; l++)
+                    {
+                        cell[i][j][k][l].setText("0");
+                    }
+                }
+            }
+        }
     }
 }
