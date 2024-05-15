6743233e1fbadccecdeab76812821f194d5ef201
Patel Khushi Sanjivbhai <155909246+Khushi130404@users.noreply.github.com>
2024-05-15 09:54:11 +0530

    Setup of difficulty Spinner

--- app/src/main/java/com/example/sudoku/HomePageActivity.java
+++ app/src/main/java/com/example/sudoku/HomePageActivity.java
@@ -5,15 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.PopupMenu;
+import android.widget.Spinner;
 
-public class HomePageActivity extends Activity
-{
+public class HomePageActivity extends Activity {
     Button btStartGame, btNewGame, btBackTracking;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
 
@@ -21,12 +25,44 @@ public class HomePageActivity extends Activity
         btNewGame = findViewById(R.id.btNewGame);
         btBackTracking = findViewById(R.id.btBackTracking);
 
+
         btNewGame.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(getApplicationContext(),NewGameActivity.class);
-                startActivity(i);
+                showPopupMenu(v);
             }
         });
     }
+
+    private void showPopupMenu(View v)
+    {
+        PopupMenu pop = new PopupMenu(getApplicationContext(),v);
+        pop.getMenuInflater().inflate(R.menu.popup_difficulty,pop.getMenu());
+
+
+        pop.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item)
+            {
+                switch (item.getTitle().toString())
+                {
+                    case "Easy" :
+                        Intent i1 = new Intent(getApplicationContext(), NewGameActivity.class);
+                        startActivity(i1);
+                        return true;
+                    case "Medium" :
+                        Intent i2 = new Intent(getApplicationContext(), NewGameActivity.class);
+                        startActivity(i2);
+                        return true;
+                    case "Hard" :
+                        Intent i3 = new Intent(getApplicationContext(), NewGameActivity.class);
+                        startActivity(i3);
+                        return true;
+                    default: return false;
+                }
+            }
+        });
+        pop.show();
+    }
+
 }
